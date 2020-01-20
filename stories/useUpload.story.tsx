480bdf196b8a4ab20b7c833048b6030fac654d85
ningliu <ningliu@sf-express.com>
2020-01-20 11:07:16 +0800

    refactor(stories): 格式化代码

--- stories/useUpload.story.tsx
+++ stories/useUpload.story.tsx
@@ -4,16 +4,26 @@ import { Button, Upload, Icon } from 'antd';
 import useUpload from '../src/useUpload/index';
 
 storiesOf('useSetState', module).add('basic usage', () => {
-  const props = useUpload({ action: 'https://www.mocky.io/v2/5cc8019d300000980a055e76', maxSize: 2 });
+  const props = useUpload({
+    action: 'https://www.mocky.io/v2/5cc8019d300000980a055e76',
+    maxSize: 2,
+  });
   const { fileResList } = props;
   return (
-     <div style={{ padding: '20px' }}>
+    <div style={{ padding: '20px' }}>
       <Upload {...props}>
         <Button>
-          <Icon type="upload" /> 点击上传
+          <Icon type='upload' /> 点击上传
         </Button>
         <div>
-          {fileResList && fileResList.map((file, index) => <img src={file.url} key={index} style={{ width: '150px', height: '150px', margin: '10px' }} />) }
+          {fileResList &&
+            fileResList.map((file, index) => (
+              <img
+                src={file.url}
+                key={index}
+                style={{ width: '150px', height: '150px', margin: '10px' }}
+              />
+            ))}
         </div>
       </Upload>
     </div>
